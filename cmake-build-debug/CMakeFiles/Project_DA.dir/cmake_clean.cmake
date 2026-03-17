a48f9cf4a3776b1ed94e64ffecedde5bf908d314
maryjmgou <up202406702@up.pt>
2026-03-17 14:46:37 +0000

    blah bkah

---
+++ cmake-build-debug/CMakeFiles/Project_DA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_DA.dir/src/main.cpp.obj"
+  "CMakeFiles/Project_DA.dir/src/main.cpp.obj.d"
+  "Project_DA.exe"
+  "Project_DA.exe.manifest"
+  "Project_DA.pdb"
+  "libProject_DA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_DA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Ana Carolina/CLionProjects/Project_DA
+# Install script for directory: C:/Users/maria/Documents/Uni/2º ano/2/DA/project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Ana Carolina/CLionProjects/Project_DA/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/maria/Documents/Uni/2º ano/2/DA/project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
